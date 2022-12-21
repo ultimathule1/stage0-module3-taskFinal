9e01ea8e689fb720f5d5bb3bf1f1a2641b50b889
Madara <ranel28021998@gmail.com>
2022-12-21 19:10:13 +0300

    It's done!

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,6 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        System.out.println(temperatureCelsius * (9 / 5.0) + 32);
+        System.out.println(temperatureCelsius * (9 / 5) + 32);
     }
 }
