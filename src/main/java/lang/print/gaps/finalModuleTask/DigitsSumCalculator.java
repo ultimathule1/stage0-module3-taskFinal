0947b219297de478486977ac5bafa6423fc803c9
Madara <ranel28021998@gmail.com>
2022-12-21 19:12:52 +0300

    It's done!

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -3,5 +3,6 @@ package lang.print.gaps.finalModuleTask;
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
         int sum = (number % 10) + (number / 10 % 10) + (number / 100 % 10) + (number / 1000);
+        System.out.println(sum);
     }
 }
