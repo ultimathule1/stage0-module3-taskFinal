f93766e021e0a6f8be32eb3cf74375ae978eb900
Madara <ranel28021998@gmail.com>
2022-12-21 19:07:37 +0300

    It's done!

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int newNumber = (number / 100) + (number % 10 * 100) + (number / 10 % 10 * 10);
+        System.out.println(newNumber);
     }
 }
